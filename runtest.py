9be720f078c608792962ff43f7a87baf44efd8ce
Fanny A Hindrarta <fanny.hindrarta@qlue.id>
2020-10-16 09:26:06 +0700

    Addition AE Region

--- runtest.py
+++ runtest.py
@@ -2,14 +2,22 @@ import cv2
 from argus_camera import ArgusCamera
 
 def main():
+    top = 250
+    left = 650
+    bottom = 850
+    right = 950
 
-    camera = ArgusCamera()
+    #camera = ArgusCamera()
+    camera = ArgusCamera(stream_resolution=(1640, 1232),
+                        video_converter_resolution=(1640, 1232),
+                        #source_clip_rect=(0.25, 0.25, 0.75, 0.75),
+                        ae_regions=[[top,left,bottom,right,0.8]])
     cv2.namedWindow("Argus Camera", cv2.WINDOW_AUTOSIZE)
-    cv2.moveWindow("Argus Camera", 50,50)
+    cv2.moveWindow("Argus Camera", 10,10)
     while True:
         image = camera.read()
-        
         bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.rectangle(bgr,(left,top),(right,bottom),(255,0,255),3)
         cv2.imshow("Argus Camera", bgr)
         k = cv2.waitKey(1)
         if k & 0xFF == 27:
