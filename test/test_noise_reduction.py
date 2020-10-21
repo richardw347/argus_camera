1877773a2a553aaff14c1aed45eaea2b92f9d1ca
Muhammad Shalahuddin Yahya Sunarko <yahya@qlue.id>
2020-10-21 13:41:42 +0700

    Test denoise

---
+++ test/test_noise_reduction.py
@@ -0,0 +1,24 @@
+import cv2
+from argus_camera import ArgusCamera
+
+
+def main():
+    camera = ArgusCamera(
+        stream_resolution=(1640, 1232),
+        video_converter_resolution=(820, 616),
+        denoise_mode=3,
+        denoise_strength=1.0)
+    cv2.namedWindow("Argus Camera", cv2.WINDOW_AUTOSIZE)
+    while True:
+        image = camera.read()
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imshow("Argus Camera", image)
+        k = cv2.waitKey(1)
+        if k & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            print("Exiting...")
+            break
+
+
+if __name__ == "__main__":
+    main()
